68c7fa3f7bb928cf0414199936926846f3407c9b
Bharat Tulsani <bharattulsani21@gmail.com>
2025-12-24 19:03:43 +0530

    next issue fix

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,13 +1,12 @@
  
-import {client} from "@repo/db/client";
-import { use } from "react";
- 
+import { client } from "@repo/db/client";
 
 export default async function Home() {
-  const user=await client.user.findFirst();
+  const user = await client.user.findFirst();
+  
   return (
-    <div >
-      {user?.username}
+    <div>
+      {user ? <p>{user.username}</p> : <p>No user found</p>}
     </div>
   );
 }
